6c643eae378dd46572b4c1a25c261711763ae084
Vakaris <aleksandravicius.vakaris07@gmail.com>
2024-10-22 14:13:09 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static int calculateRectangleArea(int height, int width){
+        int area=height*width;
+
+
+        return area;
+
+    }
+    public static void main(String[] args){
+        System.out.println(calculateRectangleArea(2,5));
+    }
+}
